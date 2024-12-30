feccf982ba575d47fd4634f213b18aa2186c8a8e
junjianni <junjian.ni@syngenta.com>
2024-12-29 22:45:27 -0500

    End of Section 6

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, inject, output, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -13,6 +14,7 @@ export class RegisterComponent {
   // usersFromHomeComponent = input.required<any>();
   // @Output() cancelRegister = new EventEmitter();
   private accountService = inject(AccountService);
+  private toastr = inject(ToastrService);
   cancelRegister = output<boolean>();
   
   model: any = {}
@@ -23,7 +25,7 @@ export class RegisterComponent {
         console.log(response);
         this.cancel();
       },
-      error: error => console.log(error)
+      error: error => this.toastr.error(error.error)
     })
   }
 
